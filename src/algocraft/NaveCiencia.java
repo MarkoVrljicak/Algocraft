f380b3233c8290364296651124e40cf20ddbcd40
AgustinDuhalde <agustinduhalde@hotmail.com>
2015-05-30 15:15:18 -0300

    Agrego NaveCiencia y NaveTransporte (no subidas anteriormente)

---
+++ src/algocraft/NaveCiencia.java
@@ -0,0 +1,8 @@
+package algocraft;
+
+public class NaveCiencia extends Unidad{
+
+	public NaveCiencia() {
+		this.vidamaxima.setVida(200);
+	}
+}
---
+++ src/algocraft/NaveTransporte.java
@@ -0,0 +1,8 @@
+package algocraft;
+
+public class NaveTransporte extends Unidad{
+
+	public NaveTransporte() {
+		this.vidamaxima.setVida(150);
+	}
+}
