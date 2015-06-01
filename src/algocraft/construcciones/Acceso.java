1bd1dabbb17fd953970fa81a00c3b11a00f5415d
nicolas7885 <nicolas7885@hotmail.com>
2015-06-01 18:49:47 -0300

    Test Acceso tiene zealot y test Acceso tiene Zealot

--- src/algocraft/construcciones/Acceso.java
+++ src/algocraft/construcciones/Acceso.java
@@ -1,13 +1,21 @@
 package algocraft.construcciones;
 
-import algocraft.creables.Creable;
+import java.util.HashMap;
 
-public class Acceso extends Construccion {
+import algocraft.creables.Creable;
+import algocraft.creables.ValidadorDeCreaciones;
+import algocraft.unidades.*;
+
+
+public class Acceso extends CreadorDeUnidades {
 
 	public Acceso() {
 		this.nombre = "Acceso";
 		this.vida.set(500);
 		this.tiempoDeConstruccion = 8;
+		unidades = new HashMap<String, ValidadorDeCreaciones>();
+		this.unidades.put("Zealot", new ValidadorDeCreaciones(100, 0, new Zealot() ));
+		this.unidades.put("Dragon", new ValidadorDeCreaciones(125,50, new Dragon() ));
 	}
 	
 	
--- test/algocraft/construcciones/AccesoTest.java
+++ test/algocraft/construcciones/AccesoTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 
 public class AccesoTest {
 	
+	private String nombreZealot="Zealot";
+	private String nombreDragon="Dragon";
+
 	@Test
 	public void testConstruyoAccesoYControloSuVidaInicial() {
 		Construccion construccion = new Acceso();
@@ -18,5 +21,19 @@ public class AccesoTest {
 		Construccion construccion = new Acceso();
 		
 		assertEquals(8, construccion.tiempoDeCreacion());
+	}
+	
+	@Test
+	public void testAccesoInicializaConZealot() {
+		Acceso acceso = new Acceso();
+		
+		assertEquals(true, acceso.tengoUnidad(nombreZealot));
+	}
+	
+	@Test
+	public void testAccesoInicializaConDragon() {
+		Acceso acceso = new Acceso();
+		
+		assertEquals(true, acceso.tengoUnidad(nombreDragon));
 	}
 }
