958e45ccb24279d362603b1b05f8836b1eb319a4
AgustinDuhalde <agustinduhalde@hotmail.com>
2015-06-01 01:09:43 -0300

    Test tiempo de construccion Puerto Estelar

--- src/algocraft/construcciones/PuertoEstelar.java
+++ src/algocraft/construcciones/PuertoEstelar.java
@@ -1,24 +1,18 @@
 package algocraft.construcciones;
 
 import algocraft.Creable;
-import algocraft.unidades.Unidad;
 
 public class PuertoEstelar extends CreadorDeUnidades {
 
 	public PuertoEstelar() {
 		this.nombre = "Puerto Estelar";
 		this.vidaMaxima.setVida(1300);
+		this.tiempoDeConstruccion = 10;
 	}
 
 	@Override
 	public Creable clonar() {
 		return (new PuertoEstelar());
-	}
-
-	@Override
-	public Unidad crearUnidad(int mineralDisponible, int gasDisponible,
-			String nombreDeUnidad) {
-		return null;
 	}
 
 }
--- test/algocraft/construcciones/PuertoEstelarTest.java
+++ test/algocraft/construcciones/PuertoEstelarTest.java
@@ -15,4 +15,10 @@ public class PuertoEstelarTest {
 		assertEquals(1300, construccion.getVida());
 	}
 
+	@Test
+	public void testVeoElTiempoDeConstruccionParaCrearPuertoEstelar() {
+		Construccion construccion = new PuertoEstelar();
+		
+		assertEquals(10, construccion.tiempoDeCreacion());
+	}
 }
