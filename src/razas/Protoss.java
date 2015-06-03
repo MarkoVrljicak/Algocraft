539e094b52f38416893976d7d636e29a3b7c62cb
AgustinDuhalde <agustinduhalde@hotmail.com>
2015-06-03 00:42:33 -0300

    Test Protoss tiene ArchivosTemplarios

--- src/razas/Protoss.java
+++ src/razas/Protoss.java
@@ -2,6 +2,7 @@ package razas;
 
 import stats.Recurso;
 import algocraft.construcciones.protos.Acceso;
+import algocraft.construcciones.protos.ArchivosTemplarios;
 import algocraft.creables.ValidadorDeRecursos;
 
 public class Protoss extends Raza{
@@ -9,6 +10,8 @@ public class Protoss extends Raza{
 	public Protoss(){
 		this.nombre = "Protoss";
 		this.construcciones.put("Acceso", new ValidadorDeRecursos(new Recurso(150,0), new Acceso() ));
+		this.construcciones.put("Archivos Templarios", new ValidadorDeRecursos(new Recurso(150,200), new ArchivosTemplarios() ));
+		
 	}
 	
 }
--- test/razas/ProtossTest.java
+++ test/razas/ProtossTest.java
@@ -7,11 +7,19 @@ import org.junit.Test;
 public class ProtossTest {
 
 	private String nombreAcceso = "Acceso";
+	private String nombreArchivosTeamplarios = "Archivos Templarios";
 
 	@Test
 	public void testProtossInicializaConAcceso() {
 		Protoss protoss = new Protoss();
 		
 		assertEquals(true, protoss.tengoConstruccion(nombreAcceso));
+	}
+	
+	@Test
+	public void testProtossInicializaConArchivosTemplarios() {
+		Protoss protoss = new Protoss();
+		
+		assertEquals(true, protoss.tengoConstruccion(nombreArchivosTeamplarios ));
 	}
 }
