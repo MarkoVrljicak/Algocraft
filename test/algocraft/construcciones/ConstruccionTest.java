24ac7153134e0295a93bf70e12582b90e3a5b6f6
AgustinDuhalde <agustinduhalde@hotmail.com>
2015-06-03 01:17:49 -0300

    Agrego test de actualizar (tiempoDeConstruccion)

--- test/algocraft/construcciones/ConstruccionTest.java
+++ test/algocraft/construcciones/ConstruccionTest.java
@@ -4,17 +4,30 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import algocraft.construcciones.terran.Barraca;
 import algocraft.construcciones.terran.Refineria;
 import algocraft.creables.Creable;
 
 public class ConstruccionTest {
 
 	@Test
-	public void ConstruccionCreaCopiaDeSiMisma() {
+	public void testConstruccionCreaCopiaDeSiMisma() {
 		Construccion construccion = new Refineria();
 		Creable refineria = construccion.clonar();
 		
 		assertEquals(construccion.getNombre() , ((Construccion)refineria).getNombre());		
 	}
 
+	@Test
+	public void testConstruccionDisminuyeTiempoDeConstruccionHastaCero() {
+		Construccion barraca = new Barraca();
+		
+		assertEquals(12, barraca.tiempoDeCreacion());
+		
+		for(int i = 0; i < 13; i++){
+			barraca.actualizar();	
+		}
+		
+		assertEquals(0, barraca.tiempoDeCreacion());
+	}
 }
