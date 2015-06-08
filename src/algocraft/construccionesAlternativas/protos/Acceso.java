f5d724e3eb65f29b0ae256b80a8bc6e467f80913
AgustinDuhalde <agustinduhalde@hotmail.com>
2015-06-08 13:44:16 -0300

    Test Acceso crea Dragon

--- src/algocraft/construccionesAlternativas/protos/Acceso.java
+++ src/algocraft/construccionesAlternativas/protos/Acceso.java
@@ -12,8 +12,12 @@ public class Acceso extends CreadorDeUnidades {
 	
 	private static final EnumEdificiosProtos nombreAcceso = EnumEdificiosProtos.ACCESO;
 	private static final UnidadesProtos nombreZealot= UnidadesProtos.ZEALOT;
+	private static final UnidadesProtos nombreDragon= UnidadesProtos.DRAGON;
 	private static final Recurso recursosNecesariosZealot = new Recurso(100,0);
+	private static final Recurso recursosNecesariosDragon = new Recurso(125,50);
 	private static final int poblacionNecesariaZealot = 2;
+	private static final int poblacionNecesariaDragon = 2;
+
 
 	public Acceso() {
 		super(new EdificioConEscudo(nombreAcceso,500,12,500));
@@ -21,12 +25,15 @@ public class Acceso extends CreadorDeUnidades {
 	
 	@Override
 	protected void determinarCreables() {
-		this.unidadesCreables.add(nombreZealot)	;
+		this.unidadesCreables.add(nombreZealot);
+		this.unidadesCreables.add(nombreDragon);
 	}
 	
 	public Unidad crearUnidad(Unidades nombreUnidad){
 		if(nombreUnidad==nombreZealot)
 			return this.crearUnidadEspecifica(nombreZealot, recursosNecesariosZealot, poblacionNecesariaZealot);
+		if(nombreUnidad==nombreDragon)
+			return this.crearUnidadEspecifica(nombreDragon, recursosNecesariosDragon, poblacionNecesariaDragon);
 		return null;//lanzar excepcion?
 			
 	}
--- test/algocraft/construccionesAlternativas/AccesoTest.java
+++ test/algocraft/construccionesAlternativas/AccesoTest.java
@@ -15,8 +15,11 @@ import algocraft.unidades.Alternativas.protos.UnidadesProtos;
 public class AccesoTest {
 	
 	private static final UnidadesProtos nombreZealot= UnidadesProtos.ZEALOT;
+	private static final UnidadesProtos nombreDragon= UnidadesProtos.DRAGON;
 	private static final Recurso recursosNecesariosZealot = new Recurso(100,0);
+	private static final Recurso recursosNecesariosDragon = new Recurso(125,50);
 	private static final int poblacionNecesariaZealot = 2;
+	private static final int poblacionNecesariaDragon = 2;
 	
 	@Test
 	public void testAccesoInicializaConZealot() {
@@ -54,6 +57,47 @@ public class AccesoTest {
 		Unidad zealot = acceso.crearUnidad(nombreZealot);
 		
 		assertEquals(nombreZealot, zealot.getNombre());
+	}
+	
+	@Test
+	public void testAccesoInicializaConDragon() {
+		Acceso acceso = new Acceso();
+		
+		assertEquals(true, acceso.tengoUnidad(nombreDragon));
+	}
+		
+	@Test
+	public void testAccesoPuedeCrearDragonConRecursosSuficientesyPoblacionSuficiente() {
+		Acceso acceso = new Acceso();
+		Jugador jugador = new Jugador("Nombre", new Terran());
+		
+		jugador.getRecursos().incrementarGas(50);
+		acceso.setDuenio(jugador);
+					
+		assertEquals(true,acceso.puedoCrearUnidad(recursosNecesariosDragon, poblacionNecesariaDragon));
+	}	
+		
+	@Test
+	public void testAccesoNoPuedeCrearDragonConRecursosInSuficientesyPoblacionSuficiente() throws RecursosNegativosException {
+		Acceso acceso = new Acceso();
+		Jugador jugador = new Jugador("Nombre", new Terran());
+		
+		acceso.setDuenio(jugador);
+		jugador.getRecursos().consumirMineral(200);
+			
+		assertEquals(false,acceso.puedoCrearUnidad(recursosNecesariosDragon, poblacionNecesariaDragon));
+	}
+		
+	@Test
+	public void testAccesoCreaDragon() {
+		Acceso acceso = new Acceso();
+		Jugador jugador = new Jugador("Nombre", new Terran());
+		
+		jugador.getRecursos().incrementarGas(50);
+		acceso.setDuenio(jugador);
+		Unidad dragon = acceso.crearUnidad(nombreDragon);
+		
+		assertEquals(nombreDragon, dragon.getNombre());
 	}
 }
 
