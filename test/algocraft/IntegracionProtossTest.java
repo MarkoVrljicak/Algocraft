27ff9d2d3e11c2e7534ecd02bfe56c8dc3e9c0b0
AgustinDuhalde <agustinduhalde@hotmail.com>
2015-06-09 16:50:51 -0300

    Agrego pruebas de integracion de Protoss

---
+++ test/algocraft/IntegracionProtossTest.java
@@ -0,0 +1,126 @@
+package algocraft;
+
+import static org.junit.Assert.*;
+import jugador.Jugador;
+import org.junit.Test;
+import razasAlternativas.Protoss;
+import algocraft.construccionesAlternativas.protos.EnumEdificiosProtos;
+
+public class IntegracionProtossTest {
+
+	private static final EnumEdificiosProtos nombreAcceso= EnumEdificiosProtos.ACCESO;
+	private static final EnumEdificiosProtos nombreArchivosTemplarios= EnumEdificiosProtos.ARCHIVOS_TEMPLARIOS;
+	private static final EnumEdificiosProtos nombreAsimilador= EnumEdificiosProtos.ASIMILADOR;
+	private static final EnumEdificiosProtos nombreNexoMineral= EnumEdificiosProtos.NEXO_MINERAL;
+	private static final EnumEdificiosProtos nombrePuertoEstelar= EnumEdificiosProtos.PUERTO_ESTELAR;
+	private static final EnumEdificiosProtos nombrePilon= EnumEdificiosProtos.PILON;
+	
+	@Test
+	public void testCrearAcceso() {
+		Jugador jugador = new Jugador("pepe", new Protoss());
+		jugador.construir(nombreAcceso);
+		
+		assertTrue(jugador.tieneConstruccion(nombreAcceso));
+	}
+	
+	@Test
+	public void testCrearAsimilador() {
+		Jugador jugador = new Jugador("pepe", new Protoss());
+		jugador.construir( nombreAsimilador);
+		
+		assertTrue(jugador.tieneConstruccion(nombreAsimilador));
+	}
+	
+	@Test
+	public void testCrearNexoMineral() {
+		Jugador jugador = new Jugador("pepe", new Protoss());
+		jugador.construir( nombreNexoMineral);
+		
+		assertTrue(jugador.tieneConstruccion(nombreNexoMineral));
+	}
+	
+	@Test
+	public void testCrearPilon() {
+		Jugador jugador = new Jugador("pepe", new Protoss());
+		jugador.construir(nombrePilon);
+		
+		assertTrue(jugador.tieneConstruccion(nombrePilon));
+	}
+
+	@Test
+	public void testCrearPuertoEstelar() {
+		Jugador jugador = new Jugador("pepe", new Protoss());
+		//construyo edificios para acumular recursos 
+		jugador.construir(nombreNexoMineral);
+		jugador.construir(nombreNexoMineral);
+		jugador.construir(nombreAsimilador);
+		//espero a que se construyan y recolecten recursos de mas
+		for(int i=1; i<= 200  ; i++){
+			jugador.pasarTurno();
+		}
+		//creo edificios necesarios
+		jugador.construir(nombreAcceso);
+		
+		//creo PuertoEstelar
+		jugador.construir(nombrePuertoEstelar);
+		
+		assertTrue(jugador.tieneConstruccion(nombrePuertoEstelar));
+	}
+	
+	@Test
+	public void testNoSeCreaPuertoEstelarSinAcceso() {
+		Jugador jugador = new Jugador("pepe", new Protoss());
+		//construyo edificios para acumular recursos 
+		jugador.construir(nombreNexoMineral);
+		jugador.construir(nombreNexoMineral);
+		jugador.construir(nombreAsimilador);
+		//espero a que se construyan y recolecten recursos de mas
+		for(int i=1; i<= 200  ; i++){
+			jugador.pasarTurno();
+		}
+				
+		//creo PuertoEstelar sin tener Acceso
+		jugador.construir(nombrePuertoEstelar);
+		
+		assertFalse(jugador.tieneConstruccion(nombrePuertoEstelar));
+	}
+	
+	@Test
+	public void testCrearArchivosTemplarios() {
+		Jugador jugador = new Jugador("pepe", new Protoss());
+		//construyo edificios para acumular recursos 
+		jugador.construir(nombreNexoMineral);
+		jugador.construir(nombreNexoMineral);
+		jugador.construir(nombreAsimilador);
+		//espero a que se construyan y recolecten recursos de mas
+		for(int i=1; i<= 200  ; i++){
+			jugador.pasarTurno();
+		}
+		//creo edificios necesarios
+		jugador.construir(nombreAcceso);
+		jugador.construir(nombrePuertoEstelar);
+		
+		//creo ArchivosTemplarios
+		jugador.construir(nombreArchivosTemplarios);
+		
+		assertTrue(jugador.tieneConstruccion(nombreArchivosTemplarios));
+	}
+	
+	@Test
+	public void testNoSeCreaArchivosTemplariosSinPuertoEstelar() {
+		Jugador jugador = new Jugador("pepe", new Protoss());
+		//construyo edificios para acumular recursos 
+		jugador.construir(nombreNexoMineral);
+		jugador.construir(nombreNexoMineral);
+		jugador.construir(nombreAsimilador);
+		//espero a que se construyan y recolecten recursos de mas
+		for(int i=1; i<= 200  ; i++){
+			jugador.pasarTurno();
+		}
+		
+		//creo ArchivosTemplarios
+		jugador.construir(nombreArchivosTemplarios);
+		
+		assertFalse(jugador.tieneConstruccion(nombreArchivosTemplarios));
+	}
+}
