fa5298a1b0f92343c70f71a3f8d1fc273a6e1013
MarkoVrljicak <marcos.vrljicak@gmail.com>
2015-06-14 17:14:25 -0300

    arreglo svn (espero)

---
+++ src/algocraft/movimientos/MovimientoAereo.java
@@ -0,0 +1,13 @@
+package algocraft.movimientos;
+
+import java.util.Collection;
+
+import algocraft.mapa.terrenos.Terreno;
+import algocraft.unidades.Unidad;
+
+public class MovimientoAereo {
+	
+	public void ejecutar(Collection<Terreno> camino, Unidad unidad){
+		
+	}
+}
---
+++ src/algocraft/movimientos/MovimientoTerrestre.java
@@ -0,0 +1,12 @@
+package algocraft.movimientos;
+
+import java.util.Collection;
+
+import algocraft.mapa.terrenos.Terreno;
+import algocraft.unidades.Unidad;
+
+public class MovimientoTerrestre {
+	public void ejecutar(Collection<Terreno> camino, Unidad unidad){
+		
+	}
+}
