f58e538f4fe9c4b70e8c3280d8b59a72514150ff
MarkoVrljicak <marcos.vrljicak@gmail.com>
2015-06-14 18:12:50 -0300

    Movimiento aereo implementado.

--- src/algocraft/movimientos/MovimientoAereo.java
+++ src/algocraft/movimientos/MovimientoAereo.java
@@ -1,13 +1,44 @@
 package algocraft.movimientos;
 
 import java.util.Collection;
+import java.util.Iterator;
 
+import algocraft.exception.DestinoInvalidoException;
+import algocraft.exception.PropiedadNoExisteEnEstaUbicacion;
 import algocraft.mapa.terrenos.Terreno;
 import algocraft.unidades.Unidad;
 
 public class MovimientoAereo extends Movimiento {
 	
 	public void ejecutar(Collection<Terreno> camino, Unidad unidad){
+		Iterator<Terreno> iter = camino.iterator();
 		
+		Terreno terrenoOriginal = null;
+		Terreno proximoTerreno = iter.next();
+		
+		while(iter.hasNext()){
+			terrenoOriginal = proximoTerreno;
+			proximoTerreno = iter.next();
+			
+			if(!proximoTerreno.sePuedeVolar() || unidad.getMovimientos() < 1){
+				break;
+				
+			} else {
+				try {
+					proximoTerreno.almacenar(unidad);
+				} catch (DestinoInvalidoException e) {
+					// No deberia, se chequeo anteriormente.
+					e.printStackTrace();
+				} 
+				
+				try {
+					terrenoOriginal.borrarContenido(unidad);
+				} catch (PropiedadNoExisteEnEstaUbicacion e) {
+					e.printStackTrace();
+				}
+				
+				unidad.darPaso();
+			}
+		}
 	}
 }
